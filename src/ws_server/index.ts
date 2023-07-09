224e9384f64f41ffaedd7234e871ae1d5024e98a
Pavel <opatriarkh@gmail.com>
2023-07-09 21:40:49 +0300

    feat: add basic user logic

--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -1,15 +1,21 @@
+import { Duplex } from 'stream';
 import { WebSocketServer } from 'ws';
+import { parseRequest } from '../helpers/parser.js'
+import { json } from 'stream/consumers';
+import { StringDecoder } from 'string_decoder';
+import { Buffer } from 'buffer';
 
 export const wss = new WebSocketServer({ port: 3000 });
 
 console.log("run server ws");
+ 
 
-wss.on('connection', function connection(ws) {
-  console.log(`new connection ${JSON.stringify(ws)}`)
+wss.on('connection', (ws) => {
+  console.log(`new connection`)
   ws.on('error', console.error);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+  ws.on('message', (rawdata:string) => {
+    console.log(JSON.parse(rawdata))
   });
 
   ws.send('something');
